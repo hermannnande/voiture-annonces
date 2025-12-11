b06f702de271b7458ce883ec0b3fc2e0c90bec77
Hermann Nande <hermannmande@gmail.com>
2025-12-11 19:29:02 +0000

    fix: DÃ©sactivation temporaire de SimilarListings pour debug

--- frontend/src/components/listings/ListingDetail.tsx
+++ frontend/src/components/listings/ListingDetail.tsx
@@ -265,8 +265,8 @@ Voir l'annonce : ${listingUrl}`;
         </div>
       </div>
 
-      {/* Annonces similaires */}
-      <SimilarListings listingId={listing.id} />
+      {/* Annonces similaires - Temporairement désactivé */}
+      {/* <SimilarListings listingId={listing.id} /> */}
     </div>
   );
 }
